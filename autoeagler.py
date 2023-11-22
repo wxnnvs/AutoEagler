3e82b12e39acafc7d57d2fc87221ff4d368fd8f1
wxnnvs <215158@proton.me>
2023-11-22 14:39:07 +0100

    add ngrok support

--- autoeagler.py
+++ autoeagler.py
@@ -2,9 +2,12 @@ import os
 import requests
 import shutil
 import time
-import threading
 import subprocess
-import psutil   
+import psutil
+import os
+
+from contextlib import redirect_stdout, redirect_stderr
+from pyngrok import conf, ngrok
 
 # URLs and file locations
 latest_bungee = "https://api.papermc.io/v2/projects/waterfall/versions/1.20/builds/540/downloads/waterfall-1.20-540.jar"
@@ -12,7 +15,7 @@ bungee_location = "Bungee/BungeeCord.jar"
 latest_eaglerx = "https://github.com/lax1dude/eagl3rxbungee-memory-leak-patch/raw/main/EaglerXBungee-Memleak-Fixed.jar"
 eaglerx_location = "./Bungee/plugins/eaglerXbungee.jar"
 latest_spigot = "https://cdn.getbukkit.org/spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar"
-spigot_location = "Server\Spigot.jar"
+spigot_location = "Server/Spigot.jar"
 
 def download_file(url, location):
     response = requests.get(url)
@@ -56,6 +59,8 @@ def run_servers():
     run_command_in_new_terminal(f'java -Xms2G -Xmx2G -jar {os.path.basename(spigot_location)}')
     os.chdir(os.path.dirname("../"))
 
+    print("Servers starting ...")
+
 def stop_servers():
     print("Stopping servers...")
     
@@ -70,12 +75,19 @@ def stop_servers():
 
     print("Servers stopped.")
 
+def open_tunnel():
+    global http_tunnel
+    conf.get_default().monitor_thread = False
+    with redirect_stdout(None) and redirect_stderr(None):
+        http_tunnel = ngrok.connect(8081, 'http', bind_tls=True, )
+    print("Server running at ", http_tunnel.public_url.replace("https", "wss"))
+
 def main():
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         print("1) Set up AutoEagler")
-        print("2) Run AutoEagler")
-        print("3) Add plugins")
+        print("2) Run locally")
+        print("3) Run with NGROK")
         print("4) Wipe everything")
         print("5) Exit")
 
@@ -126,11 +138,13 @@ def main():
 
         elif choice == '2':
             run_servers()
-            print("Servers starting ...\nJoin on ws://localhost:8081 using an offline download")
-            time.sleep(20)
+            print("Server running at ws://localhost:8081")
+            input("Press [Enter] to return to the menu (servers stay up)")
 
         elif choice == '3':
-            pass  # Add plugins logic here
+            run_servers()
+            open_tunnel()
+            input("Press [Enter] to return to the menu (servers stay up)")
 
         elif choice == '4':
             remove_everything()
@@ -138,7 +152,13 @@ def main():
 
         elif choice == '5':
             stop_servers()
-            break
+            with redirect_stdout(None) and redirect_stderr(None):
+                tunnels = ngrok.get_tunnels()
+            if not tunnels:
+                break
+            else:
+                ngrok.disconnect(http_tunnel.public_url)
+                break
 
 if __name__ == "__main__":
     main()
