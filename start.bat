13e84ebe75ead7f2847a4f876080fdaf047b36de
wxnnvs <215158@proton.me>
2023-12-02 10:03:47 +0100

    added software check + bugfix

--- start.bat
+++ start.bat
@@ -1,9 +1,23 @@
 @echo off
-title AutoEagler | Checking software ...
+title "Checking software ..."
+
+where python3 >nul 2>nul
+if %errorlevel%==1 (
+    @echo Python not found, please install the latest release from https://python.org.
+    pause
+    exit 0
+)
+
+where java >nul 2>nul
+if %errorlevel%==1 (
+    @echo Java not found, please install Java 8 from https://java.com
+    pause
+    exit 0
+)
 
 python3 -m pip install -r requirements.txt
 
-title AutoEagler | Running ...
+title "Running ..."
 
 python3 autoeagler.py
 
